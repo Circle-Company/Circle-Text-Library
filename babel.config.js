482daa5028dedf280f6c81b7f2f62705e46d5ad8
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-07-28 23:09:57 -0300

    add: babel settings

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = api => {
+  return {
+    plugins: [
+      '@babel/plugin-transform-flow-strip-types',
+      '@babel/plugin-transform-runtime',
+      '@babel/plugin-transform-spread',
+      '@babel/plugin-syntax-dynamic-import',
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-export-namespace-from',
+      '@babel/plugin-proposal-export-default-from',
+      'add-module-exports'
+    ],
+    presets: [
+      [
+        '@babel/preset-env',
+      ]
+    ]
+  };
+};
